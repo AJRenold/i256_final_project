fa118fbc25a7f9a31bc5cee4fd3dc18812413da7
David Greis <dgreis@ischool.berkeley.edu>
2013-11-13 10:40:52 -0800

    crawler now outputs to data/test/out. need to figure out to chop up this output as data gets larger

--- crawler.py
+++ crawler.py
@@ -3,13 +3,65 @@
 #crawler.py
 
 import re
+import time
+import json,os
+from cPickle import load,dump
 
+
+cwd = os.getcwd()
+dir = cwd+'/data/test/'
+
+jsonsamp = 'tweets121120130845.json'
+
+fileList = [dir+jsonsamp]
+
+
+def main():
+    Cr = ourCrawler()
+    for f in fileList:
+        o = open(f)
+        tweets = json.load(o)
+        o.close()
+        testSet = tweets[1:20]                  #This controls how big the output will be
+        testRes = Cr.crawl(testSet)
+        for k in testRes:
+            outName = dir+'/out/'+k+'.pk1'
+            outFile = open(outName,'wb')
+            dump(testRes[k],outFile)
+            outFile.close()           
+            
+            
+            
 class ourCrawler:
     from reporter import Reporter
     r = Reporter()
+    
     def __init__(self):
         self.isCrawler = True
-    def crawl(self,link,rep=r):
+       
+    def crawl(self,tweets):
+        noneList = []
+        moreList = []
+        otherProb = []
+        outDict = {}    
+        for t in tweets:
+            linktry = self.extractLink(t)
+            if linktry[1] == 'more':
+                moreList.append(t)
+            if linktry[1] == 'none':
+                noneList.append(t)
+            if linktry[1] == 'OK':
+                link = linktry[0]
+                sensFlag = t['possibly_sensitive']
+                time.sleep(.25)
+                try:
+                    linkText = self.fetchText(link)
+                    outDict[link] = [linkText,sensFlag]
+                except AttributeError:
+                    otherProb.append(t)
+        return({'noneList': noneList,'moreList':moreList,'otherProb':otherProb,'outDict':outDict})
+            
+    def fetchText(self,link,rep=r):
         rep.read(url=link)
         result = rep.report_news()
         #r = r'{.*}'
@@ -20,15 +72,31 @@ class ourCrawler:
             return None
         else:
             return(result)
+            
+    def extractLink(self,tweet):
+        text = tweet['text']
+        linkPat = '''http://'''
+        lp = re.compile(linkPat)
+        instances = lp.findall(text)
+        if len(instances) > 1:
+            print 'ALERT: MORE THAN ONE LINK FOUND'
+            return( (text,'more') )
+        if len(instances) == 0:
+            print "ALERT: DID NOT FIND ANY LINKS"
+            return( (text,'none') )
+        else:
+            st = lp.search(text).start()
+            findSpace = re.compile(''' ''')
+            yesSpace = findSpace.search(text[st:]+'')
+            if yesSpace:
+                link = text[st:yesSpace.start()+st]
+                return ( (link, 'OK') )
+            else:
+                link = text[st:]
+                return ( (link, 'OK') )
 
 
-def extractLink(text):
-    linkPat = '''http://'''
-    lp = re.compile(linkpat)
-    st = p1re.search(text).start()
-    findSpace = re.compile(''' ''')
-    yesSpace = findspace.search(text[st:]+'')
-    if yesSpace:
-        return text[st:yesSpace.start()+st]
-    else:
-        return text[st:]
+        
+main()            
+
+            
