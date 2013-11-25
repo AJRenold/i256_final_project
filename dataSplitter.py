ed6709017a6882615e75cdae77dacd1ca0ccf481
David Greis <dgreis@ischool.berkeley.edu>
2013-11-25 15:50:32 -0800

    dataSplitter now outputs pickle of indices for test and training data

--- dataSplitter.py
+++ dataSplitter.py
@@ -11,21 +11,23 @@ import math
 import random
 import os
 import sys
+from cPickle import dump,load
 import pandas as pd
 
+#testingDat = '/Users/dgreis/Documents/School/Classes/INFO_256/Outbrain/i256_final_project/data/forTesting/buzzLinkTest/RSS.csv'
 
 def main():
     try:
         rawDat = pd.read_csv(sys.argv[1])
+        #rawDat = pd.read_csv(testingDat)
     except IndexError:
         print "\nAt the moment, this script needs a command line argument which is the name of the csv file you want to split\n"
         sys.exit()
     ds = dataSplitter()
-    valDat = ds.validate(rawDat)
-    splitDat = ds.split(valDat)
+    valList = ds.validate(rawDat)
+    splitDict = ds.split(valList)
     for n in ['train','test']:
-        dat = splitDat[n]
-        ds.write(n,dat)
+        ds.write(n,splitDict)
 
 
 class dataSplitter:
@@ -48,27 +50,40 @@ class dataSplitter:
             for p in probList:
                 print str(p)
             print('*'*40)
-        valid = df.ix[keepList]
-        return(valid.reset_index(drop=True))
+        #valid = df.ix[keepList]
+        #return(valid.reset_index(drop=True))
+        return keepList
     
-    def split(self,df):
-        lenDat = len(df)
+    
+    def split(self,keepList):
+        lenDat = len(keepList)
         s = list(range(lenDat))
         random.shuffle(s)
         spl = int(math.floor(.75*lenDat))
-        tr_ind = s[0:spl]
-        te_ind = s[spl+1:]
-        tr = df.ix[tr_ind]
-        te = df.ix[te_ind]
+        randInd_tr = s[0:spl]
+        randInd_te = s[spl+1:]
+        #tr = df.ix[tr_ind]
+        #te = df.ix[te_ind]
+        tr = [keepList[i] for i in randInd_tr]
+        te = [keepList[i] for i in randInd_te]
         return ({'train': tr,'test':te})        
         
-    def write(self,prefix,df):
+    def write(self,prefix,outDict):
         ts = time.time()
         st = datetime.datetime.fromtimestamp(ts).strftime('%m-%d-%Y_%H-%M')
         cwd = os.getcwd()
-        outFile = cwd + '/data/' + prefix + '/' + prefix + '_@'+st+'.csv'
+        outName = cwd + '/data/' + prefix + '/' + prefix + '_@'+st+'.pk1'
+        '''with open(outFile, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow((prefix,'rows'))
+            for k in outDict:
+                writer.writerow((k,outDict[k]))
+        csvfile.close()
         df.to_csv(outFile)
-        print prefix + ' data successfully written ' + st
+        outName = cwd+'/allInputFiles.pk1'''
+        outFile = open(outName,'wb')
+        dump(outDict[prefix],outFile)
+        print prefix + ' data index successfully written ' + st
 
         
 if __name__ == '__main__':
